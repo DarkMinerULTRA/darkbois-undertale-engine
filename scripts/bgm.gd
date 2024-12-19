9569da2232079250c07bc5c424aa28a8971c0997
DarkMinerULTRA <henry_burton@outlook.com>
2024-12-19 14:28:43 +1030

    fixed a bgm thing!
    hehe

--- scripts/bgm.gd
+++ scripts/bgm.gd
@@ -3,8 +3,14 @@ extends Node
 var audio = AudioStreamPlayer.new()
 
 var room_audio = {
-	"room_overworldtest":"mus_ruins.ogg",
-	"room_test2":"mus_ruins.ogg"
+	"room_entrance":"mus_ruins.ogg",
+	"room_spikepuzzle1":"mus_ruins.ogg",
+	"room_tree":"mus_ruins.ogg",
+	"room_long":"mus_ruins.ogg",
+	"room_long2":"mus_ruins.ogg",
+	"room_chest":"mus_ruins.ogg",
+	"room_ghostgames":"mus_gameshow.ogg",
+	"ghosts":"mus_gathering.ogg"
 }
 
 func pause(paused : bool):
@@ -15,14 +21,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if playerVariables.next_room != "":
+		if room_audio.has(playerVariables.next_room):
+			if audio.stream != load("res://assets/music/"+room_audio[playerVariables.next_room]):
+				audio.volume_db = 0-(60*fade.get_node("fade").color.a)
+			else:
+				audio.volume_db = 0
+		else:
+			audio.volume_db = 0-(60*fade.get_node("fade").color.a)
 	if !get_tree().current_scene:
 		return
-	if get_tree().current_scene.name == data.rooms[data.savedata["curScene"]] and data.rooms[data.savedata["curScene"]] in room_audio:
-		if audio.stream == load("res://assets/music/"+room_audio[data.rooms[data.savedata["curScene"]]]):
+	if get_tree().current_scene.name in room_audio:
+		if audio.stream == load("res://assets/music/"+room_audio[get_tree().current_scene.name]):
 			return
 		else:
-			audio.stream = load("res://assets/music/"+room_audio[data.rooms[data.savedata["curScene"]]])
+			audio.stream = load("res://assets/music/"+room_audio[get_tree().current_scene.name])
 		if audio.playing == false:
 			audio.play()
 	else:
-		audio.stream_paused = true
+		audio.stream = null
